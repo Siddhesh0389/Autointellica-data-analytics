2ca44bf119a602a90b0c11e87d7ddffe9e5ae304
Siddhesh0389 <siddhesh110322@gmail.com>
2025-11-27 00:01:32 +0530

    Initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,10 @@
-// main.jsx
+// src/main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
-import './index.css'
-
-const basename = window.location.hostname === 'siddhesh0389.github.io' 
-  ? '/Autointellica-data-analytics' 
-  : '';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <BrowserRouter basename={basename}>
-      <App />
-    </BrowserRouter>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
